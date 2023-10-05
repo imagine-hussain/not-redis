261b80df5fd71bb380abaab5a4b67cf8297a3ecd
imagine-hussain <hussain.nwz000@gmail.com>
2023-10-05 22:54:44 +1100

    make it ASYNC

--- client_py/client.py
+++ client_py/client.py
@@ -1,36 +1,38 @@
 from __future__ import annotations
 import socket
-import time
 from queue import Queue
 from typing import Literal, Optional, Union
-from threading import Semaphore
 import functools
 import asyncio
 
 def yield_when_done(f, *args, **kwargs):
     @functools.wraps(f)
     async def wrapper(self: NotRedisConn, *args, **kwargs):
-        f(self, *args, **kwargs)
+        await f(self, *args, **kwargs)
         await self.yield_to_pool()
     return wrapper
 
 
 class NotRedisConnPool():
-    async def __init__(self, host: str, port: int, size: int = 8):
+    """
+    TODO: write about usage (use .initialise)
+    """
+    def __init__(self, host: str, port: int, size: int = 8):
         self.addr = (host, port)
         self._size = size
         self._sem = asyncio.Semaphore(size)
         self._connections: Queue[NotRedisConn] = Queue()
-        for _ in range(size):
-            self._connections.put(self._create_new_conn())
 
-    def _create_new_conn(self):
+    async def initialise(self):
+        for _ in range(self._size):
+            self._connections.put(await self._create_new_conn())
+
+    async def _create_new_conn(self):
         """
         Create a new connection to the server.
         Note that this does NOT add the connection to the pool.
         """
-        conn = NotRedisConn(*self.addr, self)
-        return conn
+        return await NotRedisConn(*self.addr, self).initialise()
 
     async def get_conn(self) -> NotRedisConn:
         await self._sem.acquire()
@@ -41,56 +43,70 @@ class NotRedisConnPool():
         self._sem.release()
 
 class NotRedisConn():
+    """
+    TODO: write about usage (use .initialise)
+    """
     def __init__(self, host: str, port: int, pool: NotRedisConnPool):
         self.socket = socket.create_connection((host, port))
         self.pool = pool
+        self.host = host
+        self.port = port
+
+    async def initialise(self) -> NotRedisConn:
+        # self.socket = socket.create_connection((host, port))
+        self.read, self.write = await asyncio.open_connection(self.host, self.port)
+        return self
 
     def _send(self, data: Union[bytes, bytearray]):
         # The first 4 bytes of the data is the length of the data
         data = len(data).to_bytes(4, byteorder='big') + data
-        self.socket.sendall(data)
+        self.write.write(data)
 
-    def recv(self) -> bytes:
-        size = self.socket.recv(4)
-        data = self.socket.recv(int.from_bytes(size))
-        return data
+    async def recv(self) -> bytes:
+        # size = self.socket.recv(4)
+        size = await self.read.readexactly(4)
+        return await self.read.readexactly(int.from_bytes(size))
+
+    async def recv_str(self) -> str:
+        return (await self.recv()).decode()
 
     @yield_when_done
-    def get(self, key: str) -> Optional[str]:
+    async def get(self, key: str) -> Optional[str]:
         msg = f'GET {key}'
         self._send(msg.encode())
-        return self._recv_with_arg()
+        return await self._recv_with_arg()
 
     @yield_when_done
-    def delete(self, key: str) -> Optional[str]:
+    async def delete(self, key: str) -> Optional[str]:
         msg = f'DEL {key}'
         self._send(msg.encode())
-        return self._recv_with_arg()
+        return await self._recv_with_arg()
 
     @yield_when_done
-    def set(self, key: str, value: str) -> Optional[str]:
+    async def set(self, key: str, value: str) -> Optional[str]:
         msg = f'SET {key} {value}'
         self._send(msg.encode())
-        return self._recv_with_arg()
+        return await self._recv_with_arg()
 
     @yield_when_done
-    def ping(self) -> Literal["PONG"]:
+    async def ping(self) -> Literal["PONG"]:
         msg = 'PING'
         self._send(msg.encode())
-        retval = self.recv().decode()
+        retval = str(await self.recv())
         assert retval == 'PONG'
         return retval
 
     @yield_when_done
-    def echo(self, msg: str) -> Optional[str]:
+    async def echo(self, msg: str) -> Optional[str]:
         msg = f'ECHO {msg}'
         self._send(msg.encode())
-        return self._recv_with_arg()
+        return await self._recv_with_arg()
 
-    def _recv_with_arg(self) -> Optional[str]:
+    async def _recv_with_arg(self) -> Optional[str]:
+        prev_val = await self.recv_str()
         return (
             prev_val[4:] # f'CMD {arg}' -> f'{arg}'
-            if (prev_val := self.recv().decode()) != '(nil)'
+            if prev_val != '(nil)'
             else None
         )
 
@@ -98,6 +114,5 @@ class NotRedisConn():
         await self.pool.yield_conn(self)
 
     def close(self):
-        self.socket.close()
-
+        self.write.close()
 
